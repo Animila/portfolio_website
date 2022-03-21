698ad2f1bbee7fce3081ff694ea66ce380604816
Animila <hristoforovila5@gmail.com>
2022-03-21 06:17:06 +0000

    Delete Cache

--- blogs/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Blogs(models.Model):
-    title = models.CharField(max_length=100)
-    date = models.DateField()
-    description = models.TextField()
-
-    def __str__(self):
-        return self.title
--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'news'
-
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('<int:news_id>/', views.detail, name='detail')
-]
