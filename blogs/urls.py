9ef6d00f637a265911a9f35fd0523b3d2818241e
Animila <hristoforovila5@gmail.com>
2022-04-02 16:30:37 +0300

    Delete useless app

--- blogs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'blogs'
-
-urlpatterns = [
-    path('', views.blogs, name="all_blogs"),
-    path('<int:blog_id>/', views.detail, name='detail')
-]
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,14 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-import home
-import blogs
-import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('home.urls'), name="home"),
-    path('blogs/', include('blogs.urls'), name='blogs'),
-    path('about/', include('about.urls'), name='about')
+    path('', include('home.urls'), name="home")
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
