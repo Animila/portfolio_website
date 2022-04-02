12fbf3cb5d65f1446062c68cce2ed5881a60c3c5
Animila <hristoforovila5@gmail.com>
2022-04-02 19:46:35 +0300

    Add page cabinet

--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import News
-
+from django.contrib.admin.views.decorators import staff_member_required
 
 def home(request):
     context = {
@@ -8,6 +8,14 @@ def home(request):
         'title': 'Главная',
     }
     return render(request, 'home/index.html', context)
+
+
+@staff_member_required
+def cabinet(request):
+    context = {
+        'title': 'Кабинет',
+    }
+    return render(request, 'home/cabinet.html', context)
 
 
 def detail(request, news_id):
