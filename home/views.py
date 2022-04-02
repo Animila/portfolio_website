88bc3ea6d4a5354c101f55df04d1636932060b73
Animila <hristoforovila5@gmail.com>
2022-04-02 17:11:20 +0300

    Rebield menu

--- home/views.py
+++ home/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 from .models import News
 
+
 def home(request):
-    data = News.objects.all()
-    return render(request, 'home/index.html', {'data': data})
+    context = {
+        'data': News.objects.all(),
+        'title': 'Главная',
+    }
+    return render(request, 'home/index.html', context)
+
 
 def detail(request, news_id):
-    data = get_object_or_404(News, pk=news_id)
-    return render(request, 'home/detail.html', {'data': data}) 
+    context = {
+        'data': get_object_or_404(News, pk=news_id),
+        'title': 'Статья',
+    }
+    return render(request, 'home/detail.html', context)
